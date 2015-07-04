5dd1c2cea6a28a3b4a604089bdaf4608326142bf
gebov <martin.gebov@gmail.com>
2015-07-04 23:02:37 +0300

    modified build

--- build.bat
+++ build.bat
@@ -4,5 +4,9 @@
 @if exist "%ProgramFiles(x86)%\MSBuild\12.0\bin" set PATH=%ProgramFiles(x86)%\MSBuild\12.0\bin;%PATH%
 
 rem build
-set "output=%cd%\output"
+set "output=%cd%\output\all"
 msbuild src/QuickSite.sln /t:Clean,Build /p:Configuration=Release,OutDir="%output%"
+
+copy %cd%\output\all\merged\QuickSite.exe %cd%\output\QuickSite.exe
+
+rd %output% /q /s
